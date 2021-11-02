66670c26a5826b3de7fd8ca72e5bee573c8d3ba8
Jair <jairaceves56@gmail.com>
2021-11-02 16:41:24 -0600

    implementacion del componente Formulario

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Formulario from './components/Formulario';
 import Header from './components/Header';
 
 function App() {
@@ -7,6 +8,19 @@ function App() {
       <Header 
         titulo = 'Clima React App'
       />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row"> 
+              <div className="col m6 s12">
+                <Formulario  />
+              </div>
+              <div className="col m6 s12">
+                2
+              </div>
+          </div>
+        </div>
+      </div>
 
     </>
   );
