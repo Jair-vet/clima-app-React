2d0b53a71f5f5fc98b3cd7c5f5f7dd02fcb6f38e
Jair <jairaceves56@gmail.com>
2021-11-06 13:41:33 -0600

    agregamos los PropTypes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
         if(consultar) {
           const appId = '8cb2a2c25fffd0cf5b3d621f06422cd2';
-          const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+          const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
   
           const respuesta = await fetch(url);
           const resultado = await respuesta.json();
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 
 const Formulario = ({ busqueda, guardarBusqueda, guardarConsultar }) => {
 
@@ -82,4 +83,9 @@ const Formulario = ({ busqueda, guardarBusqueda, guardarConsultar }) => {
      );
 }
  
+Formulario.propTypes = {
+    busqueda: PropTypes.object.isRequired,
+    guardarBusqueda: PropTypes.func.isRequired,
+    guardarConsultar: PropTypes.func.isRequired
+}
 export default Formulario;
